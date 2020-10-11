926fb6a8555ea9303b998269a14952095e366f80
bizdash1 <67599053+bizdash1@users.noreply.github.com>
2020-10-11 05:41:18 -0500

    Simplified fetches and updated for paypal data

--- biz_dashboard/assets/js/script.js
+++ biz_dashboard/assets/js/script.js
@@ -1,16 +1,60 @@
-fetch('stripe.json')
+fetch('/json/paypalTransaction.json')
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    appendData(data);
+    appendDataPpTran(data);
   })
   .catch(function (err) {
     console.log('error: ' + err);
   });
 
-function appendData(data) {
-  let dataSet = data;
+function appendDataPpTran(data) {
+
+  let table = document.getElementById("ppTran");
+
+  for (let transactions of data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    let th1 = document.createElement("th");
+    let text1 = document.createTextNode("Amount");
+    let th2 = document.createElement("th");
+    let text2 = document.createTextNode("Name");
+    th1.appendChild(text1);
+    row.appendChild(th1);
+    th2.appendChild(text2);
+    row.appendChild(th2);
+
+    for (let transaction_detail of transactions.transaction_details) {
+      let row = table.insertRow();
+      let cell1 = row.insertCell();
+      let text1 = document.createTextNode(transaction_detail.transaction_info.transaction_amount.value);
+      cell1.appendChild(text1);
+      let cell2 = row.insertCell();
+      let text2 = document.createTextNode(transaction_detail.payer_info.payer_name.alternate_full_name);
+      cell2.appendChild(text2);
+    }
+  }
+}
+
+// Paypal data fetch
+fetch('/json/paypalBalance.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendDataPpBal(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+function appendDataPpBal(data) {
+  let dataSet = {
+    total_balance: data[0].balances[0].total_balance.value,
+    available_balance: data[0].balances[0].available_balance.value,
+    withheld_balance: data[0].balances[0].withheld_balance.value,
+  };
 
   function generateTableHead(table, data2) {
     let thead = table.createTHead();
@@ -24,67 +68,20 @@ function appendData(data) {
   }
 
   function generateTable(table, data2) {
-    for (let element of data2) {
       let row = table.insertRow();
+    for (let element in data2) {
 
-      for (key in element) {
         let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
+        let text = document.createTextNode(data2[element]);
         cell.appendChild(text);
-      }
     }
   }
 
-  let table = document.getElementById("stripe");
-  let data2 = Object.keys(dataSet[0]);
-
-  generateTable(table, dataSet);
-  generateTableHead(table, data2);
-}
-
-// Paypal data fetch
-fetch('paypal.json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        appendData2(data);
-    })
-    .catch(function (err) {
-        console.log('error: ' + err);
-    });
-
-function appendData2(data) {
-    let dataSet = data;
-
-function generateTableHead(table, data2) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data2) {
-      let th = document.createElement("th");
-      let text = document.createTextNode(key);
-      th.appendChild(text);
-      row.appendChild(th);
-    }
-  }
-  
-  function generateTable(table, data2) {
-    for (let element of data2) {
-      let row = table.insertRow();
-
-      for (key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
-      }
-    }
-  }
-  
   let table = document.getElementById("payPal");
-  let data2 = Object.keys(dataSet[0]);
- 
-  generateTable(table, dataSet);
+  let data2 = Object.keys(dataSet);
+
   generateTableHead(table, data2);
+  generateTable(table, dataSet);
 }
 
 $(function () {
